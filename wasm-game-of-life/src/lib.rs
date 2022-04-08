a344954e1da5c6361060df122b882d4ba653e502
yceffort <yceffort@gmail.com>
2022-04-08 08:54:47 +0000

    cargo fmt

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -24,39 +24,38 @@ pub struct Universe {
 
 #[wasm_bindgen]
 impl Universe {
-       // public method. 이를 자바스크립트에서 쓸 수 있게 할 것이다.
-       pub fn tick(&mut self) {
+    // public method. 이를 자바스크립트에서 쓸 수 있게 할 것이다.
+    pub fn tick(&mut self) {
         // 현재 세포들을 모두 꺼내와서 복사해둔다.
-          let mut next = self.cells.clone();
-  
+        let mut next = self.cells.clone();
+
         // 현재 모든 셀을 순환한다.
-          for row in 0..self.height {
-              for col in 0..self.width {
-                  let idx = self.get_index(row, col);
-                  // 현재 세포
-                  let cell = self.cells[idx];
-                  // 주변 세포가 몇개나 살아 있는지 계산한다
-                  let live_neighbors = self.live_neighbor_count(row, col);
-  
-                  // 다음 셀은 다음과 같이 결정된다.
-                  let next_cell = match (cell, live_neighbors) {
-    
-                      // 규칙1. 살아있는 세포 근처에 두명 미만의 세포가 살아있다면, 죽는다.
-                      (Cell::Alive, x) if x < 2 => Cell::Dead,
-                      // 규칙 2: 살아있는 세포 규칙에 2~3의 살아있는 세포가 있다면, 산다.
-                      (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                      // 규칙3: 살아있는 이웃세포가 3 보다 많다면 죽는다
-                      (Cell::Alive, x) if x > 3 => Cell::Dead,
-                      // 규칙4: 살아있는 이웃이 정확히 3개있는 죽은세포는 살아난다.
-                      (Cell::Dead, 3) => Cell::Alive,
-                      // 그외의 다른 셀은 그대로...
-                      (otherwise, _) => otherwise,
-                  };
-                  next[idx] = next_cell;
-              }
-          }
-          self.cells = next;
-      }
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                // 현재 세포
+                let cell = self.cells[idx];
+                // 주변 세포가 몇개나 살아 있는지 계산한다
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                // 다음 셀은 다음과 같이 결정된다.
+                let next_cell = match (cell, live_neighbors) {
+                    // 규칙1. 살아있는 세포 근처에 두명 미만의 세포가 살아있다면, 죽는다.
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // 규칙 2: 살아있는 세포 규칙에 2~3의 살아있는 세포가 있다면, 산다.
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // 규칙3: 살아있는 이웃세포가 3 보다 많다면 죽는다
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // 규칙4: 살아있는 이웃이 정확히 3개있는 죽은세포는 살아난다.
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // 그외의 다른 셀은 그대로...
+                    (otherwise, _) => otherwise,
+                };
+                next[idx] = next_cell;
+            }
+        }
+        self.cells = next;
+    }
 
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
