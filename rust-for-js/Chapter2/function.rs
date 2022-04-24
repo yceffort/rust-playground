3fa9a8fd7b128ab8eacde8338cab8f6a027f9d8f
yceffort <yceffort@gmail.com>
2022-04-24 12:25:09 +0900

    포맷팅

--- rust-for-js/Chapter2/function.rs
+++ rust-for-js/Chapter2/function.rs
@@ -1,18 +1,18 @@
 fn main() {
-  println!("Hello, World!");
-  // 리턴이 없는 함수는 tuple () 암묵적으로 리턴한다.
+    println!("Hello, World!");
+    // 리턴이 없는 함수는 tuple () 암묵적으로 리턴한다.
 
-  let result = sum(10, 5);
-  println!("Result: {}", result);
+    let result = sum(10, 5);
+    println!("Result: {}", result);
 
-  // 러스트에도 익명함수가 존재한다. 람다함수 또는 클로져라고도 불리운다.
-  // |x| {x}
-  // 위 함수는 자바스크립트의 x => x 다.
+    // 러스트에도 익명함수가 존재한다. 람다함수 또는 클로져라고도 불리운다.
+    // |x| {x}
+    // 위 함수는 자바스크립트의 x => x 다.
 
-  // 과거에는 타입 annotaion이 없어도 되었는데, 요즘은 필요한듯
-  let anonymous_sum = |x: u64, y: u64| x + y;
-  let result2 = anonymous_sum(10, 5);
-  println!("Result: {}", result2);
+    // 과거에는 타입 annotaion이 없어도 되었는데, 요즘은 필요한듯
+    let anonymous_sum = |x: u64, y: u64| x + y;
+    let result2 = anonymous_sum(10, 5);
+    println!("Result: {}", result2);
 }
 
 // 아래와 같은 함수는 이제 더이상 컴파일 되지 않음
@@ -23,15 +23,12 @@ fn main() {
 
 // 이런 방식으로 선언해야 함.
 fn sum(x: u64, y: u64) -> u64 {
-  return x + y;
+    return x + y;
 }
-
 
 fn implicit_sum(x: u64, y: u64) -> u64 {
-  // return 문이 없다면 맨 마지막 표현식을 리턴으로 간주한다.
-  // ; 가 없는 이유는 표현식에 한해서 필요 없기 때문이다.
-  // 있으면 에러남
-  x + y
+    // return 문이 없다면 맨 마지막 표현식을 리턴으로 간주한다.
+    // ; 가 없는 이유는 표현식에 한해서 필요 없기 때문이다.
+    // 있으면 에러남
+    x + y
 }
-
-
