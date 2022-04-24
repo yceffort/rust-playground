55e5b6572dc0afef2155742c4b37cb0dc72a692d
yceffort <yceffort@gmail.com>
2022-04-24 13:00:49 +0900

    expressions

--- rust-for-js/Chapter2/expressions.rs
+++ rust-for-js/Chapter2/expressions.rs
@@ -49,9 +49,53 @@ fn main() {
         }
     }
 
+    // 자세한 설명은 생략한다.
     let mut counter2 = 0;
-    while counter <= 10 {
-        counter = counter + 1;
+    while counter2 <= 10 {
+        counter2 = counter2 + 1;
         println!("while {counter}");
     }
+
+    // 자바스크립트의 for in과 유사함.
+    for planet in [
+        "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune",
+    ] {
+        println!("{}", planet)
+    }
+
+    // 자바스크립트에 switch가 있다면, 러스트에는 패턴 매칭이 있다.
+    // let status = Status::Connected;
+    let status = Status::Fail(String::from("접속에 실패하였습니다."));
+
+    // 반드시 모든 enum을 커버해야 한다.
+    match status {
+        Status::Connected => {
+            println!("Connected!")
+        }
+        Status::DisConnected => {
+            println!("Disconnected!")
+        }
+        Status::Fail(error) => {
+            println!("Error: {}", error)
+        }
+    }
+
+    // 만약에 하나에 대해서만 구현하고싶다면 이렇게 하면 된다.
+    // match status {
+    //   Status::Fail(error) => {
+    //     println!("Error: {}", error);
+    //   }
+    //   _ => {}
+    // }
+    // 위와 동일
+    // if let Status::Fail(error) = status {
+    //   println!("Error: {}", error)
+    // }
+}
+
+enum Status {
+    Connected,
+    DisConnected,
+    // 만약 여기에 새로운 값이 추가되면 match에서 에러가 발생한다.
+    Fail(String),
 }
