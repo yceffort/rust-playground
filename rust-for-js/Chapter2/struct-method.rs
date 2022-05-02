50bf4d7521d2062da294de3caa8595dd7d5afbf3
yceffort <yceffort@gmail.com>
2022-05-02 21:31:13 +0900

    struct-method

--- rust-for-js/Chapter2/struct-method.rs
+++ rust-for-js/Chapter2/struct-method.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct URL {
     protocol: String,
     hostname: String,
@@ -5,9 +6,28 @@ struct URL {
 }
 
 // struct를 구현할때 사용
+
 impl URL {
-    fn toString(&self) -> String {
+    // &self는 자신을 가리킨다
+    // &는 참조값
+    // self는 this
+    fn to_string(&self) -> String {
         format!("{}://{}/{}", self.protocol, self.hostname, self.pathname)
+    }
+
+    fn from(url: &str) -> URL {
+        let string = String::from(url);
+        let vec: Vec<&str> = string.split("://").collect();
+        let protocol = String::from(vec[0]);
+        let rest = String::from(vec[1]);
+        let vec2: Vec<&str> = rest.split("/").collect();
+        let hostname = String::from(vec2[0]);
+        let pathname = String::from(vec2[1]);
+        URL {
+            protocol,
+            hostname,
+            pathname,
+        }
     }
 }
 
@@ -17,5 +37,9 @@ fn main() {
         hostname: String::from("yceffort.kr"),
         pathname: String::from("2022/04/chrome-memory-profiler"),
     };
-    println!("{}", app.toString());
+    println!("{}", app.to_string());
+
+    let app2 = URL::from("https://yceffort.kr/2022/04/chrome-memory-profiler");
+    println!("{:?}", app2);
+    println!("{}", app2.to_string());
 }
